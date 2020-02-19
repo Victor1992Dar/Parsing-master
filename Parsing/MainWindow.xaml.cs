b2ce80e866b91265b731c1b8fc7f2e374fcf8f8f
8700K <8700K@DESKTOP-8UEV819>
2020-02-19 22:13:17 +0300

    Введены города, области. Остался массив стран.

--- Parsing/MainWindow.xaml.cs
+++ Parsing/MainWindow.xaml.cs
@@ -25,69 +25,131 @@ namespace Parsing
 		{
 			InitializeComponent();
         }
-        Dictionary<string, string> CityName_urlWeather = new Dictionary<string, string>();
+        Dictionary<string, string> CityName_urlWeather = new Dictionary<string, string>(); //города
+		Dictionary<string, string> AreaName_urlWeather = new Dictionary<string, string>(); //области
 
-        private void StartParse_Click(object sender, RoutedEventArgs e)
+		private void StartParse_Click(object sender, RoutedEventArgs e)
 		{
-            UpdateBase("https://yandex.ru/pogoda/region/1");
-        }
+			UpdateAreaName_urlWeather("https://yandex.ru/pogoda/region/225");
+			UpdateCityName_urlWeather(AreaName_urlWeather["Алтайский край"]);
+		}
 
-        public void UpdateBase(string urlRegion)
+		public void UpdateCityName_urlWeather(string urlArea)
+		#region Функция заполнения словаря Город:ссылка на погоду
+		{
+			var startUrl = "https://yandex.ru";
+			var addUrl = "";
+			var urlWeather = "";
+
+			var url = urlArea; //Регион Москва и МО
+
+			var web = new HtmlWeb();
+			var doc = web.Load(url);
+
+			for (int iterAZ = 3; iterAZ < 36; iterAZ++)
+			{
+				try
+				{
+					for (int iterLeftRight = 1; iterLeftRight < 3; iterLeftRight++)
+					{
+						try
+						{
+							for (int iterTopDown = 1; iterTopDown < 30; iterTopDown++)
+							{
+								var htmlNodes = doc.DocumentNode.SelectNodes($"/html/body/div/div[5]/div[{iterAZ}]/ul[{iterLeftRight}]/li[{iterTopDown}]/a"); //Горки в url
+								string CityName = "";
+								foreach (var node in htmlNodes)
+								{
+									CityName = node.InnerText;
+									tbOut.Text += CityName.ToString() + "\n"; //текст между <>
+									CityName_urlWeather.Add(CityName, "");
+
+									addUrl = node.Attributes["href"].Value.ToString(); //Добавочная сылка на страницу погоды Горки
+									tbOut.Text += "Добавочная ссылка: " + addUrl + "\n";
+								}
+
+								urlWeather = startUrl + addUrl; //Полный адрес ссылки на погоду Горки      
+								CityName_urlWeather[CityName] = urlWeather;
+								tbOut.Text += $"Полный адрес ссылки на {CityName}: " + urlWeather + "\n";
+
+							}
+						}
+						catch
+						{
+							continue;
+						}
+					}
+				}
+				catch
+				{
+					continue;
+				}
+			}
+		}
+		#endregion
+		public void UpdateAreaName_urlWeather(string urlCountry)
+		#region Функция заполнения словаря Область:ссылка на область
+		{
+			var startUrl = "https://yandex.ru";
+			var addUrl = "";
+			var urlArea = "";
+
+			var url = urlCountry; //Страна
+
+			var web = new HtmlWeb();
+			var doc = web.Load(url);
+
+			for (int iterAZ = 3; iterAZ < 36; iterAZ++)
+			{
+				try
+				{
+					for (int iterLeftRight = 1; iterLeftRight < 3; iterLeftRight++)
+					{
+						try
+						{
+							for (int iterTopDown = 1; iterTopDown < 30; iterTopDown++)
+							{
+								var htmlNodes = doc.DocumentNode.SelectNodes($"/html/body/div/div[5]/div[{iterAZ}]/ul[{iterLeftRight}]/li[{iterTopDown}]/a"); //Область в url
+								string AreaName = "";
+								foreach (var node in htmlNodes)
+								{
+									AreaName = node.InnerText;
+									tbOut.Text += AreaName.ToString() + "\n"; //текст между <>
+									AreaName_urlWeather.Add(AreaName, "");
+
+									addUrl = node.Attributes["href"].Value.ToString(); //Добавочная сылка на страницу Области
+									tbOut.Text += "Добавочная ссылка: " + addUrl + "\n";
+								}
+
+								urlArea = startUrl + addUrl; //Полный адрес ссылки на Область      
+								AreaName_urlWeather[AreaName] = urlArea;
+								tbOut.Text += $"Полный адрес ссылки на {AreaName}: " + urlArea + "\n";
+
+							}
+						}
+						catch
+						{
+							continue;
+						}
+					}
+				}
+				catch
+				{
+					continue;
+				}
+			}
+		}
+		#endregion
+
+
+		private void Bt_test_Click(object sender, RoutedEventArgs e)
         {
-            var startUrl = "https://yandex.ru";
-            var addUrl = "";
-            var urlWeather = "";
+			//MessageBox.Show(CityName_urlWeather["Москва"]);		
+			var webWeather = new HtmlWeb();
+			var docWeather = webWeather.Load(CityName_urlWeather["Красногорск"]);
+			var tempNow = docWeather.DocumentNode.SelectNodes("/html/body/div[1]/div[6]/div[1]/div/div[2]/div[1]/div[5]/a//div[1]/span[1]");
 
-            var url = urlRegion; //Регион Москва и МО
-
-            var web = new HtmlWeb();
-            var doc = web.Load(url);
-
-            for (int iterAZ = 3; iterAZ < 36; iterAZ++)
-            {
-                try
-                {
-                    for (int iterLeftRight = 1; iterLeftRight <= 2; iterLeftRight++)
-                    {
-                        for (int iterTopDown = 1; iterTopDown < 30; iterTopDown++)
-                        {
-                            var htmlNodes = doc.DocumentNode.SelectNodes($"/html/body/div/div[5]/div[{iterAZ}]/ul[{iterTopDown}]/li[{iterLeftRight}]/a"); //Горки в url
-                            string CityName = " ";
-                            foreach (var node in htmlNodes)
-                            {
-                                CityName = node.InnerText;
-                                tbOut.Text += node.InnerText.ToString() + "\n"; //текст между <>
-                                CityName_urlWeather.Add(node.InnerText, "");
-
-                                addUrl = node.Attributes["href"].Value.ToString(); //Добавочная сылка на страницу погоды Горки
-                                tbOut.Text += "Добавочная ссылка: " + addUrl + "\n";
-                            }
-
-                            urlWeather = startUrl + addUrl; //Полный адрес ссылки на погоду Горки      
-                            CityName_urlWeather[CityName] = urlWeather;
-                            tbOut.Text += "Полный адрес ссылки на Авсюнино: " + urlWeather + "\n";
-
-
-                            //Работа с температурой на странице погоды Города.
-                            var webWeather = new HtmlWeb();
-                            var docWeather = webWeather.Load(urlWeather);
-
-                            var tempNow = docWeather.DocumentNode.SelectNodes("/html/body/div[1]/div[6]/div[1]/div/div[2]/div[1]/div[5]/a//div[1]/span[1]"); //Температура
-                            tbOut.Text += "Температура сейчас " + tempNow[0].InnerText + "\n";
-                        }
-                    }
-                }
-                catch
-                {
-                    continue;
-                }
-            }
-        }
-
-        private void Bt_test_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(CityName_urlWeather["Москва"]);
-            MessageBox.Show(CityName_urlWeather["Красногорск"]);
+			MessageBox.Show("Температура сейчас " + tempNow[0].InnerText);
             MessageBox.Show(CityName_urlWeather["Авсюнино"]);
         }
     }
